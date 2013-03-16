17b7d358b6fc7bfd6d4ab38bf7c1fc52b30124ec
Max Khardin <max.khardin@gmail.com>
2013-03-16 05:37:41 +0400

    Messing with initial usage of http-conduit

--- main.hs
+++ main.hs
@@ -1,2 +1,13 @@
-main = do
-	putStrLn "Fun!"
+import Data.Conduit.Binary (sinkFile)
+import Network.HTTP.Conduit
+import qualified Data.Conduit as C
+import Network
+
+api_key = "FILDTEOIK2HBORODV"
+
+main :: IO ()
+main = withSocketsDo $ do
+     request' <- parseUrl $ "http://developer.echonest.com/api/v4/artist/profile?name=weezer" ++ "&api_key=" ++ api_key
+     let request = request' { checkStatus = \_ _ _-> Nothing }
+     res <- withManager $ httpLbs request
+     print res
